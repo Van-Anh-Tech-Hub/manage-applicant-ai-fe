21fef83f3017e0342f892df0ca82622e639e05b1
NguyenDuy <nguyennduy1708@gmail.com>
2024-11-28 08:42:44 +0700

    Feat(layout): completed layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,41 +12,41 @@ import { AuthGuard } from '#/shared/hook/auth-guard'
 const roboto = Roboto({ subsets: ['latin'], weight: ['400', '500', '700'] })
 
 export const metadata: Metadata = {
-  title: 'JobCV',
-  description: 'Job for Applicant',
+    title: 'JobCV',
+    description: 'Job for Applicant',
 }
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode
+    children: React.ReactNode
 }>) {
-  return (
-    <html lang="en">
-      <body className={clsx(roboto.className)}>
-        <ConfigProvider
-          theme={{
-            token: {
-              colorPrimary: '#00b14f',
-              colorSuccess: '#28a745',
-              colorWarning: '#ffc107',
-              colorError: '#da4538',
-              colorInfo: '#17a2b8',
-              colorBgLayout: '#f8f9fa',
-              colorText: '#343a40',
-            },
-          }}
-        >
-          <ApolloClientProvider>
-            <AuthProvider>
-              <AuthGuard>
-                <ToastContainer />
-                {children}
-              </AuthGuard>
-            </AuthProvider>
-          </ApolloClientProvider>
-        </ConfigProvider>
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={clsx(roboto.className)}>
+                <ConfigProvider
+                    theme={{
+                        token: {
+                            colorPrimary: '#00b14f',
+                            colorSuccess: '#28a745',
+                            colorWarning: '#ffc107',
+                            colorError: '#da4538',
+                            colorInfo: '#17a2b8',
+                            colorBgLayout: '#f8f9fa',
+                            colorText: '#343a40',
+                        },
+                    }}
+                >
+                    <ApolloClientProvider>
+                        <AuthProvider>
+                            <AuthGuard>
+                                <ToastContainer />
+                                {children}
+                            </AuthGuard>
+                        </AuthProvider>
+                    </ApolloClientProvider>
+                </ConfigProvider>
+            </body>
+        </html>
+    )
 }
