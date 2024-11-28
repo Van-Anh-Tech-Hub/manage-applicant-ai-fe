7f4b5ccad1c1aa7faaf42d6d05587c4de495cccd
NguyenDuy <nguyennduy1708@gmail.com>
2024-11-28 07:59:56 +0700

    Feat(layout): add layout candidate

--- public/assets/icons/index.ts
+++ public/assets/icons/index.ts
@@ -1,40 +1,52 @@
-import { FiEye, FiEyeOff } from 'react-icons/fi'
-import { MdEmail } from 'react-icons/md'
-import { RiLockPasswordFill } from 'react-icons/ri'
-import { FaGoogle } from 'react-icons/fa6'
-import { FaBuildingUser } from 'react-icons/fa6'
-import { IoMdSettings } from 'react-icons/io'
-import { MdDashboard } from 'react-icons/md'
-import { MdOutlineKeyboardArrowLeft } from 'react-icons/md'
-import { CiCircleCheck } from 'react-icons/ci'
-import { SiReaddotcv } from 'react-icons/si'
-import { IoIosCamera } from 'react-icons/io'
-import { FaPlus } from 'react-icons/fa'
-import { FaUpload } from 'react-icons/fa'
-import { MdWork } from 'react-icons/md'
-import { RiBuildingFill } from 'react-icons/ri'
-import { AiFillContacts } from 'react-icons/ai'
-import { MdTipsAndUpdates } from 'react-icons/md'
-import { IoIosClose } from 'react-icons/io'
+import { FiEye, FiEyeOff } from 'react-icons/fi';
+import { MdEmail } from 'react-icons/md';
+import { RiLockPasswordFill } from 'react-icons/ri';
+import { FaGoogle } from 'react-icons/fa6';
+import { FaBuildingUser } from 'react-icons/fa6';
+import { IoMdSettings } from 'react-icons/io';
+import { MdDashboard } from 'react-icons/md';
+import { MdOutlineKeyboardArrowLeft } from 'react-icons/md';
+import { CiCircleCheck } from 'react-icons/ci';
+import { SiReaddotcv } from 'react-icons/si';
+import { IoIosCamera } from 'react-icons/io';
+import { FaPlus } from 'react-icons/fa';
+import { FaUpload } from 'react-icons/fa';
+import { MdWork } from 'react-icons/md';
+import { RiBuildingFill } from 'react-icons/ri';
+import { AiFillContacts } from 'react-icons/ai';
+import { MdTipsAndUpdates } from 'react-icons/md';
+import { IoIosClose } from 'react-icons/io';
+import { MdFolderDelete } from 'react-icons/md';
+import { TiLocation } from 'react-icons/ti';
+import { FaUser } from 'react-icons/fa';
+import { VscGitStashApply } from 'react-icons/vsc';
+import { FaEye } from 'react-icons/fa';
+import { RiMoneyDollarCircleLine } from 'react-icons/ri';
 
 export const Icons = {
-  FiEye,
-  FiEyeOff,
-  MdEmail,
-  RiLockPasswordFill,
-  FaGoogle,
-  CompanyUser: FaBuildingUser,
-  Setting: IoMdSettings,
-  Dashboard: MdDashboard,
-  ArrowLeft: MdOutlineKeyboardArrowLeft,
-  Check: CiCircleCheck,
-  CV: SiReaddotcv,
-  Camera: IoIosCamera,
-  Plus: FaPlus,
-  Upload: FaUpload,
-  Work: MdWork,
-  Company: RiBuildingFill,
-  Contact: AiFillContacts,
-  Tip: MdTipsAndUpdates,
-  Close: IoIosClose,
-}
+    FiEye,
+    FiEyeOff,
+    MdEmail,
+    RiLockPasswordFill,
+    FaGoogle,
+    CompanyUser: FaBuildingUser,
+    Setting: IoMdSettings,
+    Dashboard: MdDashboard,
+    ArrowLeft: MdOutlineKeyboardArrowLeft,
+    Check: CiCircleCheck,
+    CV: SiReaddotcv,
+    Camera: IoIosCamera,
+    Plus: FaPlus,
+    Upload: FaUpload,
+    Work: MdWork,
+    Company: RiBuildingFill,
+    Contact: AiFillContacts,
+    Tip: MdTipsAndUpdates,
+    Close: IoIosClose,
+    FileDelete: MdFolderDelete,
+    Location: TiLocation,
+    User: FaUser,
+    Apply: VscGitStashApply,
+    Eye: FaEye,
+    Money: RiMoneyDollarCircleLine,
+};
--- src/shared/layouts/candidate/routes.tsx
+++ src/shared/layouts/candidate/routes.tsx
@@ -2,30 +2,36 @@ import { Icons } from '#/icons'
 import { I_PathItem } from '#/shared/typescript'
 
 export const ROOTS = {
-  CANDIDATE: '/candidate',
+    CANDIDATE: '/candidate',
 }
 
 export const PATHS: {
-  HOME: I_PathItem[]
+    HOME: I_PathItem[]
 } = {
-  HOME: [
-    {
-      key: '1',
-      label: 'Thông tin',
-      icon: <Icons.Dashboard />,
-      path: `${ROOTS.CANDIDATE}/profile-user`,
-    },
-    {
-      key: '2',
-      label: 'Sơ yếu lý lịch',
-      icon: <Icons.CV />,
-      path: `${ROOTS.CANDIDATE}/cv-user`,
-    },
-    {
-      key: '3',
-      label: 'Cài đặt',
-      icon: <Icons.Setting />,
-      path: `${ROOTS.CANDIDATE}/settings`,
-    },
-  ],
+    HOME: [
+        {
+            key: '1',
+            label: 'Thông tin',
+            icon: <Icons.Dashboard />,
+            path: `${ROOTS.CANDIDATE}/profile-user`,
+        },
+        {
+            key: '2',
+            label: 'Sơ yếu lý lịch',
+            icon: <Icons.CV />,
+            path: `${ROOTS.CANDIDATE}/cv-user`,
+        },
+        {
+            key: '3',
+            label: 'Công việc ứng tuyển',
+            icon: <Icons.Apply />,
+            path: `${ROOTS.CANDIDATE}/applications-user`,
+        },
+        {
+            key: '4',
+            label: 'Cài đặt',
+            icon: <Icons.Setting />,
+            path: `${ROOTS.CANDIDATE}/settings`,
+        },
+    ],
 }
