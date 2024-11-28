7a971fe411768ffc4243d7eb7ba074e8a1834e51
NguyenDuy <nguyennduy1708@gmail.com>
2024-11-28 08:34:32 +0700

    Feat(layout): add layout recruiter

--- src/shared/layouts/recruiter/index.tsx
+++ src/shared/layouts/recruiter/index.tsx
@@ -8,26 +8,26 @@ import { Icons } from '#/icons'
 import { I_Children } from '#/shared/typescript'
 
 export const RecruiterLayout = ({ children }: I_Children) => {
-  const [collapsed, setCollapsed] = useState(false)
+    const [collapsed, setCollapsed] = useState(false)
 
-  return (
-    <div className="flex">
-      <SideBar collapsed={collapsed} setCollapsed={setCollapsed} />
+    return (
+        <div className="flex">
+            <SideBar collapsed={collapsed} setCollapsed={setCollapsed} />
 
-      <div className="flex-1 p-1 bg-gray-100 min-h-screen">
-        <Button
-          className="border-none ml-1"
-          icon={
-            collapsed ? (
-              <Icons.ArrowLeft className="rotate-180" size={25} />
-            ) : (
-              <Icons.ArrowLeft size={25} />
-            )
-          }
-          onClick={() => setCollapsed(!collapsed)}
-        />
-        <main className="p-4">{children}</main>
-      </div>
-    </div>
-  )
+            <div className="flex-1 p-1 bg-gray-100 min-h-screen">
+                <Button
+                    className="border-none ml-1"
+                    icon={
+                        collapsed ? (
+                            <Icons.ArrowLeft className="rotate-180" size={25} />
+                        ) : (
+                            <Icons.ArrowLeft size={25} />
+                        )
+                    }
+                    onClick={() => setCollapsed(!collapsed)}
+                />
+                <main className="p-4">{children}</main>
+            </div>
+        </div>
+    )
 }
--- src/shared/layouts/recruiter/routes.tsx
+++ src/shared/layouts/recruiter/routes.tsx
@@ -2,30 +2,30 @@ import { Icons } from '#/icons'
 import { I_PathItem } from '#/shared/typescript'
 
 export const ROOTS = {
-  RECRUITER: '/recruiter',
+    RECRUITER: '/recruiter',
 }
 
 export const PATHS: {
-  HOME: I_PathItem[]
+    HOME: I_PathItem[]
 } = {
-  HOME: [
-    {
-      key: '1',
-      label: 'Bảng tin',
-      icon: <Icons.Dashboard />,
-      path: `${ROOTS.RECRUITER}`,
-    },
-    {
-      key: '2',
-      label: 'Thông tin CTY',
-      icon: <Icons.CompanyUser />,
-      path: `${ROOTS.RECRUITER}/profile-company`,
-    },
-    {
-      key: '3',
-      label: 'Cài đặt',
-      icon: <Icons.Setting />,
-      path: `${ROOTS.RECRUITER}/setting`,
-    },
-  ],
+    HOME: [
+        {
+            key: '1',
+            label: 'Bảng tin',
+            icon: <Icons.Dashboard />,
+            path: `${ROOTS.RECRUITER}`,
+        },
+        {
+            key: '2',
+            label: 'Thông tin CTY',
+            icon: <Icons.CompanyUser />,
+            path: `${ROOTS.RECRUITER}/profile-company`,
+        },
+        {
+            key: '3',
+            label: 'Cài đặt',
+            icon: <Icons.Setting />,
+            path: `${ROOTS.RECRUITER}/setting`,
+        },
+    ],
 }
